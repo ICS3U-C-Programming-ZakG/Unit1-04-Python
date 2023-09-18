22b1404b97cb2931ea09333780482b870c87a055
Zak Goneau <zak.goneau@stu.ocsb.ca>
2023-09-18 22:07:38 +0000

    Did basic math and calculated the area and perimeter of a rectangle.

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Zak Goneau
+# Created on: Sep. 18, 2023
+# This program calculates the perimeter and area of a rectangle with dimensions of 8.9cm x 4.5cm.
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("8.9cm x 4.5cm")
+    print("")
+    print("The area is {}cm^2.".format(8.9 * 4.5))
+    print("The perimeter is {}cm.".format(2 * (8.9 + 4.5)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Zak Goneau
+# Created on: Sep. 18, 2023
+# This program does basic math.
+
+
+def main():
+    print("34+26= {}".format(34 + 26))
+    print("17-12= {}".format(17 - 12))
+    print("6*5= {}".format(6 * 5))
+    print("23/2= {}".format(23 / 2))
+    print("4**6= {}".format(4**6))
+    print("100**0.5= {}".format(100**0.5))
+
+
+if __name__ == "__main__":
+    main()
